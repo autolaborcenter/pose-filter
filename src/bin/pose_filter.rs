e847a51464abf20ce6074e60b2d355c4cd205213
YdrMaster <ydrml@hotmail.com>
2021-09-02 13:54:36 +0800

    optimize: 解析字符串
    test: 解析字符串

--- src/bin/pose_filter.rs
+++ src/bin/pose_filter.rs
@@ -1,21 +1,4 @@
-use std::ops::Mul;
-
 use nalgebra::{Isometry2, Vector2};
-
-/// 从字符串解析等距映射
-fn parse_isometry2(s: &str) -> Option<Isometry2<f64>> {
-    let numbers: Vec<_> = s
-        .split(',')
-        .map(|s| s.trim().parse::<f64>())
-        .take_while(|r| r.is_ok())
-        .map(|r| r.unwrap())
-        .collect();
-    if numbers.len() == 3 {
-        Some(Isometry2::new(Vector2::new(numbers[0], numbers[1]), numbers[2]))
-    } else {
-        None
-    }
-}
 
 fn main() {
     let mut isometry = Isometry2::new(Vector2::new(0.0, 0.0), 0.0);
@@ -43,7 +26,7 @@ fn main() {
                     // [Q]uery -> [A]nswer
                     "Q" => if words.len() == 2 {
                         let a = match parse_isometry2(words[1]) {
-                            Some(q) => { isometry.mul(q) }
+                            Some(q) => { &isometry * q }
                             None => continue
                         };
                         println!("A {},{},{}", a.translation.x, a.translation.y, a.rotation.angle())
@@ -55,4 +38,23 @@ fn main() {
             Err(_) => break
         }
     }
+}
+
+/// 从字符串解析等距映射
+fn parse_isometry2(s: &str) -> Option<Isometry2<f64>> {
+    let mut i = 0;
+    let mut numbers = [0.0; 3];
+    for r in s.split(',').map(|s| s.trim().parse::<f64>()) {
+        if i >= numbers.len() || r.is_err() { return None; }
+        numbers[i] = r.unwrap();
+        i += 1;
+    }
+    Some(Isometry2::new(Vector2::new(numbers[0], numbers[1]), numbers[2]))
+}
+
+#[test]
+fn parse_test() {
+    assert_eq!(parse_isometry2("-1,+2,-0"), Some(Isometry2::new(Vector2::new(-1.0, 2.0), 0.0)));
+    assert_eq!(parse_isometry2("1,2,3,x"), None);
+    assert_eq!(parse_isometry2(""), None);
 }
